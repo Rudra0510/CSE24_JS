4aa7399fcb5b627395d48e1388cd52baa5c59c4e
Rudra0510 <rudra.25b01011198@abes.ac.in>
2026-02-24 15:58:18 +0530

    link js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-console.log("Hello World");
-document.write("Hello World");
+// console.log("Hello World");
+// document.write("Hello World");
+
+// var num=undefined;
+// console.log(num);
+// console.log(typeof(num));
+// num=false;
+
+const sym1=Symbol(4)
+ console.log(sym1);
+ const sym2=Symbol(4);
+ console.log(sym2);
+ if(sym1==sym2){
+    console.log("true");
+ }
+ else{
+    console.log("false")
+ }
