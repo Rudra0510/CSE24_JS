5957c223df40b266a74a74cb9e7ef06edc319960
Rudra0510 <rudra.25b01011198@abes.ac.in>
2026-02-24 16:27:50 +0530

    assignment

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// console.log("Hello World");
-// document.write("Hello World");
+console.log("Hello World");
+document.write("Hello World");
 
-// var num=undefined;
-// console.log(num);
-// console.log(typeof(num));
-// num=false;
+var num=undefined;
+console.log(num);
+console.log(typeof(num));
+num=false;
 
 const sym1=Symbol(4)
  console.log(sym1);
@@ -16,3 +16,19 @@ const sym1=Symbol(4)
  else{
     console.log("false")
  }
+
+
+
+//logical operator
+
+ x=6;
+ y=3;
+ console.log(x=10&&y>1);
+ console.log(x==6||y==5);
+ console.log(!(x==y));
+
+var count;
+document.write("Starting Loop");
+for(count=0; count<10; count++){
+    document.write("Current Count : "+count+"<br/>");
+}
